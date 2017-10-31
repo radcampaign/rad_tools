6a47d9f8c5250450406d5000569d22fae22b82f8
Xaq Rothman <xaq@radcampaign.com>
2017-10-31 17:15:01 -0400

    Add some helper functions.

---
+++ src/RadTools.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\rad_tools;
+
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * A collection of utility functions for Rad developers.
+ */
+class RadTools {
+
+  /**
+   * A shortcut for entity queries.
+   *
+   * @param string $bundle
+   *   A string representing the name of a bundle. A node type if $node is
+   *   omitted.
+   * @param string $entity_type
+   *   A string representing the type of entity to be queried. Defaults to
+   *   'node'.
+   *
+   * @return \Drupal\Core\Entity\Query\QueryInterface
+   *   The query object that can query the given entity type, prepared with a
+   *   bundle condition.
+   */
+  public static function entityQuery(string $bundle, string $entity_type = 'node') {
+    return \Drupal::entityQuery($entity_type)->condition('bundle', $bundle);
+  }
+
+  /**
+   * Return the first value of a field on an entity.
+   *
+   * @param Drupal\Core\Entity\EntityInterface $entity
+   *   The entity containing the field value.
+   * @param string $field_name
+   *   The name of the field to get the value of.
+   * @param string|bool $column
+   *   The name of the column containing the field's value. Defaults to 'value',
+   *   pass FALSE to return the item.
+   *
+   * @return string|array
+   *   The first value of the field for the entity, or if $column is FALSE, the
+   *   first item for the field.
+   */
+  public static function getFirstValue(EntityInterface $entity, string $field_name, string $column = 'value') {
+    $value = $entity->get($field_name)->first()->getValue();
+    // Set column to FALSE to return the array.
+    if ($column) {
+      return $value[$column];
+    }
+    return $value;
+  }
+
+}
