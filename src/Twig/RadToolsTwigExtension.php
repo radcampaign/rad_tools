63ee08287afddc0d53f0eefd741160ce85f20768
dimezilla <diamond.joshh@gmail.com>
2017-11-27 09:32:41 -0500

    commenting and making better

--- src/Twig/RadToolsTwigExtension.php
+++ src/Twig/RadToolsTwigExtension.php
@@ -5,6 +5,7 @@ namespace Drupal\rad_tools\Twig;
 use Drupal\rad_tools\RadTools as Tools;
 
 class RadToolsTwigExtension extends \Twig_Extension {
+
   /**
    * {@inheritdoc}
    * @return string
@@ -23,6 +24,14 @@ class RadToolsTwigExtension extends \Twig_Extension {
     ];
   }
 
+  /**
+   * A helper function for getFunctions. Builds our Twig_SimleFunction
+   * instance for us.
+   * @param  string  $funcName The name of the function. Must correllate to public function
+   *                           in this class.
+   * @param  boolean $safe     Whether or not the output can be sent directly to html.
+   * @return Twig_SimpleFunction Out goe sthe simpleFunction instance.
+   */
   private function makeSimpleFunc($funcName, $safe = true) {
     $args = [];
     if($safe) {
@@ -35,12 +44,22 @@ class RadToolsTwigExtension extends \Twig_Extension {
       );
   }
 
+  /**
+   * A debug helper function for twig templates.
+   * Prints out an array and gets useful info about the values for us.
+   * @param  array $array An array we want to inspect.
+   * @param  string $name  Optional - the name of the variable from twig.
+   * @return string        The full output html to print to the screen.
+   */
   public function rdebug($array, $name = '') {
-
-    $html = "<br><div>$name KEYS: </div><br>";
+    $html = '<div class="rdebug-wrapper">';
+    $html .= "<br><div>$name KEYS: </div><br>";
     if (is_array($array)) {
       foreach($array as $key => $value) {
         $gInstance = Tools::getInstance($value);
+        // if our instance is an array or string
+        // we can go ahead and extract some helpful info
+        // and print it out.
         switch($gInstance) {
           case 'array':
              $gInstance .= '[' . count($value) . ']';
@@ -54,10 +73,20 @@ class RadToolsTwigExtension extends \Twig_Extension {
         $html .= '<br>';
       }
     }
+    else {
+      $html .= json_encode($array);
+    }
+    $html .= '</div>';
 
     return $html;
   }
 
+  /**
+   * Prints out what the variable is. Since drupal is inheritance hell, this
+   * helps a lot in figuring out what to google.
+   * @param  any $object Something to pass along.
+   * @return string         The type of instance.
+   */
   public function get_instance($object) {
     return Tools::getInstance($object);
   }
