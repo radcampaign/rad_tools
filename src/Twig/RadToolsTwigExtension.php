73648068daacd5b9a2878cb0f88c74ad42955098
dimezilla <diamond.joshh@gmail.com>
2017-11-27 11:45:28 -0500

    making rdebug prettier and adding some js open/close/remove events

--- src/Twig/RadToolsTwigExtension.php
+++ src/Twig/RadToolsTwigExtension.php
@@ -52,8 +52,49 @@ class RadToolsTwigExtension extends \Twig_Extension {
    * @return string        The full output html to print to the screen.
    */
   public function rdebug($array, $name = '') {
-    $html = '<div class="rdebug-wrapper">';
-    $html .= "<br><div>$name KEYS: </div><br>";
+    $id = rand();
+    $html = '';
+
+    $html .= "
+      <style type=\"text/css\">
+      #rdebug-$id {
+        position: fixed;
+        top: 5px;
+        left: 5px;
+        border: 1px solid black;
+        border-radius: 5%;
+        background-color: white;
+        padding: 15px;
+        z-index: 9999;
+      }
+      #rdebug-$id .rdebug-title {
+        font-weight: bold;
+        cursor: pointer;
+        margin: 10px 0;
+      }
+      #rdebug-$id button {
+        margin: 10px 0;
+      }
+      </style>
+      <script type=\"text/javascript\">
+        function RDebugHideThis$id(elem) {
+          var children = elem.childNodes;
+          for(var i = 0; i < children.length; i++) {
+            if(children[i].className == 'rdebug-values') {
+              children[i].style.display = children[i].style.display == 'none' ? 'block' : 'none';
+            }
+          }
+        }
+
+        function RDebugCloseThis$id(id) {
+          document.getElementById(id).remove();
+        }
+      </script>
+    ";
+    $html .= '<div class="rdebug-wrapper" id="rdebug-' . $id . '" onclick="RDebugHideThis' . $id .'(this)">'; // start wrapper
+
+    $html .= "<div class=\"rdebug-title\">$name KEYS: </div>";
+    $html .= '<div class="rdebug-values">';
     if (is_array($array)) {
       foreach($array as $key => $value) {
         $gInstance = Tools::getInstance($value);
@@ -77,6 +118,8 @@ class RadToolsTwigExtension extends \Twig_Extension {
       $html .= json_encode($array);
     }
     $html .= '</div>';
+    $html .= '<div><button onclick="RDebugCloseThis' . $id . '(\'rdebug-' . $id . '\')">Remove</button></div>';
+    $html .= '</div>'; // end class rdebug-wrapper
 
     return $html;
   }
